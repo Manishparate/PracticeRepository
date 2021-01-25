26be9b945b89fda352b6b0dfdab0d4a8ed9cc4f5
TL <manis@LAPTOP-Manish>
2021-01-25 12:55:24 +0530

    tl() method added in Test class

--- Proj1/src/com/manish/demo/Test.java
+++ Proj1/src/com/manish/demo/Test.java
@@ -4,9 +4,15 @@ public class Test {
 	public static void dev1() {
 		System.out.println("from dev1");
 	}
+
+	public static void tl() {
+		System.out.println("from dev1");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("from Dev1");
 		System.out.println("from TL");
 		dev1();
+		tl();
 	}
 }
