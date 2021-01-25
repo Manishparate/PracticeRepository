c0f1397e0a8bbd92e73a7572776f3125b54f82f9
Dev1 <manis@LAPTOP-Manish>
2021-01-25 12:46:31 +0530

    dev1() method added in Test class

--- Proj1/src/com/manish/demo/Test.java
+++ Proj1/src/com/manish/demo/Test.java
@@ -1,8 +1,12 @@
 package com.manish.demo;
 
 public class Test {
+	public static void dev1() {
+		System.out.println("from dev1");
+	}
 	public static void main(String[] args) {
 		System.out.println("from Dev1");
 		System.out.println("from TL");
+		dev1();
 	}
 }
